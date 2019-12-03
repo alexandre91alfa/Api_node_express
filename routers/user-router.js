7e5afb356d30cb75dbcbe1311fcff81393e6ccd1
Alexandre <alexandre.alfa@yahoo.com>
2019-12-02 21:19:48 -0300

    incluindo put e delete

--- routers/user-router.js
+++ routers/user-router.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const { join } = require("path");
 
+function compare(a, b) {
+  if (a.id < b.id) return -1;
+  if (a.id > b.id) return 1;
+  return 0;
+}
+
 const filePath = join(__dirname, "users.json");
 
 const getUsers = () => {
@@ -17,7 +23,9 @@ const saveUser = user =>
 
 const userRoute = app => {
   app.route("/users/:id?").get((req, resp) => {
-    const users = getUsers();
+    const id = req.params.id - 1;
+    let users = !Number.isNaN(id) ? getUsers()[id] : getUsers();
+    users = users.sort(compare);
     resp.send({ users });
   });
   app.route("/insert").post((req, resp) => {
@@ -26,8 +34,30 @@ const userRoute = app => {
     saveUser(users);
     resp.status(201).send("OK");
   });
-  app.route("/up").put((req, resp) => {
+  app.route("/update/:id?").put((req, resp) => {
     const users = getUsers();
+    const id = req.params.id;
+
+    saveUser(
+      users.map(user => {
+        if (user.id === id) {
+          return {
+            ...user,
+            ...req.body
+          };
+        }
+        return user;
+      })
+    );
+    resp.status(201).send("OK");
+  });
+  app.route("/del/:id?").delete((req, resp) => {
+    const users = getUsers();
+    const id = req.params.id;
+
+    const v = users.filter(user => user.id !== id);
+    saveUser(v);
+    resp.status(201).send("OK");
   });
 };
 
